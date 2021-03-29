e1813c0e633b4e325d92df17fe559ad994b31ac4
Chetan <61468014+Unknown39825@users.noreply.github.com>
2021-03-29 22:06:28 +0530

    few corrections

--- TestFront/src/components/CreateEvent.jsx
+++ TestFront/src/components/CreateEvent.jsx
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Button, Container, Form } from 'react-bootstrap'
 import { Redirect } from 'react-router-dom';
 
