67ddb135d49adab41a8dfff03f97fcfc5a554f41
Harshit Sharma <harshitsharmabtp@gmail.com>
2021-03-31 09:57:30 +0530

    userSchema added

---
+++ models/User/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    firstname: {
+        type: String,
+          default: ''
+      },
+      lastname: {
+        type: String,
+          default: ''
+      },
+      admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+User.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', UserSchema);
