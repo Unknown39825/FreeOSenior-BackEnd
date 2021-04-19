b7685e0e93ec06cc5c871d7b22b6ecdf3c2aae1f
Chetan <61468014+Unknown39825@users.noreply.github.com>
2021-04-19 11:11:49 +0530

    fix to delete route

--- routes/ProjectNotes/projectcard.js
+++ routes/ProjectNotes/projectcard.js
@@ -7,7 +7,7 @@ router.post("/projectcard",authenticate.verifyUser,createProjectCards);
 router.get("/projectcard",getProjectCards);
 router.get("/projectcard/:cardId",authenticate.verifyUser,getProjectCardsbyId);
 router.put("/projectcard/:cardId",authenticate.verifyUser,updateProjectCards);
-router.delete("/projectcard/:cardId",authenticate.verifyUser,deleteProjectCards);
+router.delete("/projectcard/:cardId",authenticate.verifyUser,authenticate.verifyAdmin,deleteProjectCards);
 router.put("/markprojectcard/:cardId",authenticate.verifyUser,markProjectCards);    
 //TODO : one user should mark only once 
 
