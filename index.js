edf44398455eb837c5c576a577d71d13157f2553
Chetan <61468014+Unknown39825@users.noreply.github.com>
2021-04-20 11:33:04 +0530

    cors check

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.urlencoded({ extended: false }));
 mongoose.Promise = global.Promise;
 
